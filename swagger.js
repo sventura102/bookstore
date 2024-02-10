936cca3769575f374adc698c9fe5f90257fa6fef
Stephanie <sventura102@gmail.com>
2024-02-09 20:49:12 -0800

    05 personal assignment

---
+++ swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Book Collectioon API'
+  },
+  host: 'https://books-z7vs.onrender.com/',
+  schemes: ['https']
+};
+
+const outputFile = './swagger-output.json';
+const routes = ['./routes/index'];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen(outputFile, routes, doc);
