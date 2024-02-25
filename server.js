03f27db6562d44f482a43837c9c74a7766099dd4
Stephanie <sventura102@gmail.com>
2024-02-24 22:31:18 -0800

    authentication

--- server.js
+++ server.js
@@ -5,13 +5,27 @@ const port = 3000
 const mongodb = require('./db/connect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'iOXpWdlBFpoo15jmwMPGYkCwyNC7QQy1',
+  issuerBaseURL: 'https://dev-lk7oot47rptfzc7y.us.auth0.com'
+};
 
 app
+  .use(auth(config))
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
+  })
+  .get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
   })
   .use('/', require('./routes'));
 
